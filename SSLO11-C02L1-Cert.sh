27ed35e2b8b53f62470b01f0e0aa13be5ed890fc
djlamey <76072289+djlamey@users.noreply.github.com>
2024-08-19 15:20:45 -0400

    Update SSLO11-C02L1-Cert.sh

--- SSLO11-C02L1-Cert.sh
+++ SSLO11-C02L1-Cert.sh
@@ -10,21 +10,21 @@ curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/$ucs1   
 curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/$ucs2     --output /tmp/$ucs2
 
 # confirm sslo1 is active
-for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "NO-LICENSE" ] && break; sleep 5; done
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "NO-LICENSE" ] && break; sleep 10; done
 
 #prepare sslo1
-sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
-sudo ssh 192.168.1.31 tmsh load sys ucs $ucs
+sudo scp /tmp/$ucs1 192.168.1.31:/var/local/ucs
+sudo ssh 192.168.1.31 tmsh load sys ucs $ucs1
 
 # confirm sslo2 is active
-for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "NO-LICENSE" ] && break; sleep 5; done
+for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "NO-LICENSE" ] && break; sleep 10; done
 
 #prepare sslo2
 sudo scp /tmp/$ucs2 192.168.2.31:/var/local/ucs
 sudo ssh 192.168.2.31 tmsh load sys ucs $ucs2
 
 # update Student Workstation
-touch /tmp/lab4.1
+touch /tmp/Lab2-Cert
 sudo ip route change default via 172.16.1.33
 
 # confirm bigip1 is active
