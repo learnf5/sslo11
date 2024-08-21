a2096e8808908c67a1e5412453a454db42ec49e1
djlamey <76072289+djlamey@users.noreply.github.com>
2024-08-21 09:03:01 -0400

    Update and rename SSLO-C04L1-TranFwdPrx.sh to SSLO11-C04L1-TranFwdPrx.sh

--- SSLO11-C04L1-TranFwdPrx.sh
+++ SSLO11-C04L1-TranFwdPrx.sh
@@ -4,8 +4,8 @@ PS4='+$(date +"%T.%3N"): '
 
 #Download needed files
 ucs=sslo1_cert.ucs
-curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
-curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs     --output /tmp/$ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/$ucs     --output /tmp/$ucs
 
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
