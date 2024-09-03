ee370fc3e3129a8b556b6078433f63e38144844a
djlamey <76072289+djlamey@users.noreply.github.com>
2024-09-03 15:16:25 -0500

    Create SSLO11-C08L3-FwdPrxAuth.sh

---
+++ SSLO11-C08L3-FwdPrxAuth.sh
@@ -0,0 +1,35 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+#Download needed files
+ucs=sslo1_tls13.ucs
+scf=swg_profile.scf
+#curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/$ucs --output /tmp/$ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/scf/$scf --output /tmp/$scf
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+#prepare sslo1
+sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
+sudo scp /tmp/$scf 192.168.1.31:/var/local/scf
+sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
+sudo ssh 192.168.1.31 tmsh modify sys provision apm level minimum
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# merge in swg_profile configs
+sudo ssh 192.168.1.31 tmsh load sys config merge file $scf
+sudo ssh 192.168.1.31 tmsh modify /apm profile access SWG_Profile generation-action increment
+sudo ssh 192.168.1.31 tmsh save sys config
+#sudo ssh 192.168.1.31 reboot
+
+# update Student Workstation
+touch /tmp/lab8.3
+sudo ip route change default via 172.16.1.33
+
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
