48261fa5ce7ae73c3c4532ec24c0bfce5907c90a
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-19 15:59:08 -0500

    Update and rename SSLO11-C08L3-FwdPrxAuth.sh to SSLO11-Lab08-Policies.sh

--- SSLO11-Lab08-Policies.sh
+++ SSLO11-Lab08-Policies.sh
@@ -16,6 +16,11 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
 sudo scp /tmp/$scf 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# provision APM
 sudo ssh 192.168.1.31 tmsh modify sys provision apm level minimum
 
 # confirm sslo1 is active
@@ -23,13 +28,19 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 
 # merge in swg_profile configs
 sudo ssh 192.168.1.31 tmsh load sys config merge file $scf
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# create SWG_Profile
 sudo ssh 192.168.1.31 tmsh modify /apm profile access SWG_Profile generation-action increment
 sudo ssh 192.168.1.31 tmsh save sys config
 #sudo ssh 192.168.1.31 reboot
 
 # update Student Workstation
-touch /tmp/lab8.3
-sudo ip route change default via 172.16.1.33
+touch /tmp/lab8
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+set +x
