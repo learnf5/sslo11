b6109518f3634f83084b2747e5b7f8d9ca7a8b9c
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-19 15:37:37 -0500

    Update and rename SSLO11-C02L1-Cert.sh to SSLO11-Lab02-Cert.sh

--- SSLO11-Lab02-Cert.sh
+++ SSLO11-Lab02-Cert.sh
@@ -7,10 +7,9 @@ mkdir /home/student/Desktop/Lab_Files
 curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
 
 # update Student Workstation
-touch /tmp/lab2.1
-sudo ip route change default via 172.16.1.33
+touch /tmp/lab2
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
-# NOTHING specific for this lab - only the common lab files from common-tasks.sh
+set +x
