47d353e6bc3aeb31361992a1aedfda7b5a4efc78
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-19 16:01:09 -0500

    Update and rename SSLO11-C09L1-TS.sh to SSLO11-Lab09-TS.sh

--- SSLO11-Lab09-TS.sh
+++ SSLO11-Lab09-TS.sh
@@ -3,7 +3,7 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 #Download needed files
-ucs=sslo1_tls13.ucs
+ucs=sslo1_in_app.ucs
 #curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
 curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/$ucs     --output /tmp/$ucs
 
@@ -15,8 +15,7 @@ sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
 sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
 
 # update Student Workstation
-touch /tmp/lab9.1
-sudo ip route change default via 172.16.1.33
+touch /tmp/lab9
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
