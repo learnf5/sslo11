5bede3e8fdde4c71468f32b6f9f2e14010257851
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-21 16:47:26 -0500

    Create SSLO11-Lab03-Upgrade.sh

---
+++ SSLO11-Lab03-Upgrade.sh
@@ -0,0 +1,25 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+#Download needed files
+rpm=f5-iappslx-ssl-orchestrator-17.1.1-11.1.7.noarch.rpm
+md5=f5-iappslx-ssl-orchestrator-17.1.1-11.1.7.noarch.rpm.md5
+ucs=sslo1_tf_proxy.ucs
+#mkdir --parents /home/student/Documents/SSLO/rpm
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/$rpm --output /home/student/Desktop/Lab_Files/$rpm
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/$md5 --output /home/student/Desktop/Lab_Files/$md5
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/$md5 --output /tmp/$ucs
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+#prepare sslo1
+sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
+sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
+
+# update Student Workstation
+touch /tmp/lab3
+
+# disable debugging
+set +x
