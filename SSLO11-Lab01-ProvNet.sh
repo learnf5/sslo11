1bac6af7af74ecea875d7a4704ec372646b8e404
djlamey <76072289+djlamey@users.noreply.github.com>
2025-10-24 12:25:09 -0400

    Update SSLO11-Lab01-ProvNet.sh
    added sudo to ssh commands

--- SSLO11-Lab01-ProvNet.sh
+++ SSLO11-Lab01-ProvNet.sh
@@ -6,10 +6,10 @@ PS4='+$(date +"%T.%3N"): '
 touch /tmp/lab1
 
 # Rename virtual server in base config
-ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value true
-ssh root@192.168.1.31 tmsh mv ltm virtual existing_app_pool existing_app_vs
-ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value false
-ssh root@192.168.1.31 tmsh save /sys config
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value true
+sudo ssh root@192.168.1.31 tmsh mv ltm virtual existing_app_pool existing_app_vs
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value false
+sudo ssh root@192.168.1.31 tmsh save /sys config
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
