1e1f4e943d95140f9358d57256f1c516bf1dfff6
djlamey <76072289+djlamey@users.noreply.github.com>
2025-10-24 11:14:08 -0400

    Update SSLO11-Lab10-Ha.sh
    added more ucs files to download, installing prep_ha

--- SSLO11-Lab10-Ha.sh
+++ SSLO11-Lab10-Ha.sh
@@ -6,20 +6,24 @@ PS4='+$(date +"%T.%3N"): '
 ucs1=sslo1_ha.ucs
 ucs2=sslo2_ha.ucs
 #curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
-curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/$ucs1     --output /tmp/$ucs1
-curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/$ucs2     --output /tmp/$ucs2
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/sslo1_prep_ha.ucs     --output /tmp/sslo1_prep_ha.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/sslo1_ha.ucs     --output /tmp/sslo1_ha.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/sslo1_ha_L3.ucs     --output /tmp/sslo1_ha_L3.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/sslo2_prep_ha.ucs     --output /tmp/sslo2_prep_ha.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/sslo2_ha.ucs     --output /tmp/sslo2_ha.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/ucs/sslo2_ha_L3.ucs     --output /tmp/sslo2_ha_L3.ucs
 
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
 #prepare sslo1
-sudo scp /tmp/$ucs1 192.168.1.31:/var/local/ucs
-sudo ssh 192.168.1.31 tmsh load sys ucs $ucs1 no-license
+sudo scp /tmp/sslo1_*.ucs 192.168.1.31:/var/local/ucs
+sudo ssh 192.168.1.31 tmsh load sys ucs sslo1_prep_ha.ucs no-license
 
 #prepare sslo2
-sudo scp /tmp/$ucs2 192.168.2.31:/var/local/ucs
-sudo ssh 192.168.2.31 tmsh load sys ucs $ucs2 no-license
+sudo scp /tmp/sslo2_*.ucs 192.168.2.31:/var/local/ucs
+sudo ssh 192.168.2.31 tmsh load sys ucs sslo1_prep_ha.ucs no-license
 
 # update Student Workstation
 touch /tmp/lab10
