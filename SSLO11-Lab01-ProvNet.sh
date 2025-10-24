021ab655ade662b920a74d06c81d0955ee8e9eba
djlamey <76072289+djlamey@users.noreply.github.com>
2025-10-24 12:57:58 -0400

    Update SSLO11-Lab01-ProvNet.sh
    added check for Active BIG-IP before virtual vs rename

--- SSLO11-Lab01-ProvNet.sh
+++ SSLO11-Lab01-ProvNet.sh
@@ -5,6 +5,9 @@ PS4='+$(date +"%T.%3N"): '
 # update Student Workstation
 touch /tmp/lab1
 
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
 # Rename virtual server in base config
 sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value true
 sudo ssh root@192.168.1.31 tmsh mv ltm virtual existing_app_pool existing_app_vs
