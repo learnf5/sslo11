3228e439053d4a0b96ded3489a946fd3be27ea3c
djlamey <76072289+djlamey@users.noreply.github.com>
2025-10-24 11:35:36 -0400

    Update SSLO11-Lab01-ProvNet.sh
    added step to rename existing_app_pool to existing_app_vs

--- SSLO11-Lab01-ProvNet.sh
+++ SSLO11-Lab01-ProvNet.sh
@@ -5,5 +5,14 @@ PS4='+$(date +"%T.%3N"): '
 # update Student Workstation
 touch /tmp/lab1
 
+# Rename virtual server in base config
+ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value true
+ssh root@192.168.1.31 tmsh mv ltm virtual existing_app_pool existing_app_vs
+ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value false
+ssh root@192.168.1.31 tmsh save /sys config
+
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
 # disable debugging
 set +x
