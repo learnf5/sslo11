713e62bf6121a04cc0212ac3b35dac1da5dda85d
djlamey <76072289+djlamey@users.noreply.github.com>
2025-10-24 13:31:06 -0400

    Update SSLO11-Lab10-Ha.sh
    added code to rename existing_app_pool > existing_app_vs

--- SSLO11-Lab10-Ha.sh
+++ SSLO11-Lab10-Ha.sh
@@ -32,4 +32,13 @@ touch /tmp/lab10
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
+# Rename virtual server in base config
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value true
+sudo ssh root@192.168.1.31 tmsh mv ltm virtual existing_app_pool existing_app_vs
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value false
+sudo ssh root@192.168.1.31 tmsh save /sys config
+
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
 set +x
