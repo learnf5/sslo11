a47ba7f021c4b71de08f223d5dcfff5309e977f8
djlamey <76072289+djlamey@users.noreply.github.com>
2025-10-24 13:20:35 -0400

    Update SSLO11-Lab06-Topos.sh
    added code to rename existing_app_pool > existing_app_vs

--- SSLO11-Lab06-Topos.sh
+++ SSLO11-Lab06-Topos.sh
@@ -14,6 +14,15 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
 sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
 
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# Rename virtual server in base config
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value true
+sudo ssh root@192.168.1.31 tmsh mv ltm virtual existing_app_pool existing_app_vs
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value false
+sudo ssh root@192.168.1.31 tmsh save /sys config
+
 # update Student Workstation
 touch /tmp/lab6
 
