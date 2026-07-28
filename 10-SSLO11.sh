1dbc1a6df38396c27da94697077a8007793e2a9e
djlamey <76072289+djlamey@users.noreply.github.com>
2026-07-28 09:12:32 -0400

    Update 10-SSLO11.sh
    added common actions from old script

--- 10-SSLO11.sh
+++ 10-SSLO11.sh
@@ -1,2 +1,11 @@
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# Rename virtual server in base config
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value true
+sudo ssh root@192.168.1.31 tmsh mv ltm virtual existing_app_pool existing_app_vs
+sudo ssh root@192.168.1.31 tmsh modify /sys db mcpd.mvenabled value false
+sudo ssh root@192.168.1.31 tmsh save /sys config
+
+# confirm bigip1 is active
+for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
