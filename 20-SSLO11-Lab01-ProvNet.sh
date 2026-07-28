4258b92eb80539063d9d0d5f27cf27716da1f385
djlamey <76072289+djlamey@users.noreply.github.com>
2026-07-28 09:45:52 -0400

    Update 20-SSLO11-Lab01-ProvNet.sh
    removed enable - disable debug lines

--- 20-SSLO11-Lab01-ProvNet.sh
+++ 20-SSLO11-Lab01-ProvNet.sh
@@ -1,12 +1,5 @@
-# enable debugging
-set -x
-PS4='+$(date +"%T.%3N"): '
-
 # update Student Workstation
 touch /tmp/lab1
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
-
-# disable debugging
-set +x
