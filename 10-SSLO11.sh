c71e8c67ae8ec2735b2c85102a18db444e228241
djlamey <76072289+djlamey@users.noreply.github.com>
2026-07-28 09:43:55 -0400

    Update 10-SSLO11.sh
    added enable debug at start of file

--- 10-SSLO11.sh
+++ 10-SSLO11.sh
@@ -1,3 +1,7 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
