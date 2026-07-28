5f2676c446f331a133ac180d61c82b2d13d0494c
djlamey <76072289+djlamey@users.noreply.github.com>
2026-07-28 09:45:08 -0400

    Update 30-SSLO11.sh
    removed enable debug and PS4= lines at start of file

--- 30-SSLO11.sh
+++ 30-SSLO11.sh
@@ -1,11 +1,3 @@
-# enable debugging
-set -x
-PS4='+$(date +"%T.%3N"): '
-
-# run this lab's specific tasks saved on GitHub
-#curl --silent --output /tmp/$LAB_ID.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$LAB_ID.sh
-#bash -x /tmp/$LAB_ID.sh
-
 # add copy-paste file on desktop
 curl --silent https://raw.githubusercontent.com/learnf5/sslo11/main/sslo11_copy_paste.txt --output /home/student/Desktop/Copy-Paste.txt
 
